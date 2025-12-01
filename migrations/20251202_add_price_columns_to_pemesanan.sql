1457d6016852ce0f18d99e615691245602654582
NamaAnda <nakulasaputra08@gmail.com>
2025-12-02 02:58:18 +0700

    Update banyakkkkkkkk

--- migrations/20251202_add_price_columns_to_pemesanan.sql
+++
@@ -1,6 +0,0 @@
--- Add price columns to pemesanan so totals can be stored
-ALTER TABLE `pemesanan`
-  ADD COLUMN `harga_tiket` decimal(10,0) DEFAULT 0,
-  ADD COLUMN `harga_porter` decimal(10,0) DEFAULT 0,
-  ADD COLUMN `harga_alat` decimal(10,0) DEFAULT 0,
-  ADD COLUMN `total_harga` decimal(12,0) DEFAULT 0;

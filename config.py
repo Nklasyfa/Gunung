e8570b17b0a4bb263daabf85e9ecf3c85a496f78
NamaAnda <nakulasaputra08@gmail.com>
2025-12-02 07:27:47 +0700

    Update benerin bug sebelum di deploy

--- config.py
+++ config.py
@@ -5,7 +5,4 @@ class Config:
     MYSQL_DB = 'pemesanan_tiket_gunung' 
     MYSQL_CURSORCLASS = 'DictCursor'
     
-    # Google Maps API Key
-    # Dapatkan dari: https://console.cloud.google.com/
-    # Pastikan API yang diaktifkan: Maps JavaScript API, Geocoding API
-    GOOGLE_MAPS_API_KEY = 'YOUR_GOOGLE_MAPS_API_KEY_HERE'  # Ganti dengan API key Anda
+    
